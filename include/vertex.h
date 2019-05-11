8659a83528d534fe4e4ffe7c5960d3644de673fd
Ronaldd Pinho <ronaldppinho@gmail.com>
2019-05-11 01:00:20 -0300

    Add implementations for Edges

--- include/vertex.h
+++ include/vertex.h
@@ -1,22 +1,57 @@
 #ifndef _vertex_
 #define _vertex_
 
+#include <iostream>
+using std::ostream;
 #include <string>
+#include "Valuable.h"
 
-class Vertex {
+// Class for vertex representation, contains id, name, degree values
+class Vertex : public Valuable<float> {
+
 public:
     int id;
     std::string name;
-    int degree; // vertex degree
-    bool valuated; // define if vertex has value
-    float value;
+    int in_degree, out_degree; // vertex degree
 
-    Vertex (int __id): id(__id), name(std::to_string(__id)), valuated(false), value((float)0) {};
+    Vertex (int __id): id(__id), name(std::to_string(__id)), out_degree(0), in_degree(0), Valuable() {};
 
-    Vertex (std::string __name): id(0), name(__name), valuated(false), value((float)0) {};
+    Vertex (std::string __name): id(0), name(__name), out_degree(0), in_degree(0), Valuable() {};
 
     ~Vertex () {};
 
+    int degree() {
+        return (in_degree + out_degree);
+    }
+
+    // to print on output stream using <<
+    friend ostream& operator<< (ostream &_os, const Vertex &_vert) {
+        _os << _vert.name;
+        return _os;
+    };
+
+};
+
+
+// Class to represent Edges, contains the vertices and values info
+class Edge : public std::pair<Vertex, Vertex>, public Valuable<float> {
+public:
+    using Valuable::valued;
+    using Valuable::set_value;
+    typedef Vertex V;
+
+    bool directed;
+    
+    // constructor, extends from std::pair and Valuable
+    // gets two Vertex objects
+    Edge (V __v1, V __v2) : directed(false), std::pair<V, V>(__v1, __v2), Valuable() {};
+
+    ~Edge () {};
+
+    friend ostream& operator<< (ostream &__os, const Edge &__edge) {
+        __os << "(" << __edge.first.name << ", " << __edge.second.name << ")";
+        return __os;
+    };
 };
 
 #endif
