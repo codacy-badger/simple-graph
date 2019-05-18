5a86733d0e7ec7ff823536f70a0a712480b0925e
Ronaldd Pinho <ronaldppinho@gmail.com>
2019-05-17 23:46:40 -0300

    Adcionou novos construtores e métodos

--- include/ve_descriptors.hpp
+++ include/ve_descriptors.hpp
@@ -7,18 +7,20 @@ using std::ostream;
 #include <vector>
 #include "Valuable.hpp"
 
+
 // Class for vertex representation, contains id, name, degree values
-class Vertex : public Valuable<float> {
-
+class Vertex : Valuable<double> {
 public:
-    int id;
-    std::string name;
+    using Valuable::valued;
+    using Valuable::is_valued;
+
+    int id; // number id of the vertex
+    std::string name; // vertex name
     int in_degree, out_degree; // vertex degree
-    std::vector<Vertex> v_adjacents;
 
-    Vertex (int __id): id(__id), name(std::to_string(__id)), out_degree(0), in_degree(0), v_adjacents(std::vector<Vertex>()), Valuable() {};
+    Vertex (int __id): id(__id), name(std::to_string(__id)), out_degree(0), in_degree(0), Valuable() {};
 
-    Vertex (std::string __name): id(0), name(__name), out_degree(0), in_degree(0), v_adjacents(std::vector<Vertex>()), Valuable() {};
+    Vertex (std::string __name): id(0), name(__name), out_degree(0), in_degree(0), Valuable() {};
 
     ~Vertex () {};
 
@@ -32,21 +34,33 @@ public:
         return _os;
     };
 
+    // Comparison Operators:
+    // compare two vertices
+    friend bool operator==(const Vertex& v, const Vertex& u) {
+        return (v.id == u.id);
+    };
+
+    // compare int value with vertex, use id (int)
+    friend bool operator==(const Vertex& v, const int u) {
+        return (v.id == u);
+    };
 };
+
 
 
 // Class to represent Edges, contains the vertices and values info
 class Edge : public std::pair<Vertex, Vertex>, public Valuable<float> {
 public:
     using Valuable::valued;
-    using Valuable::set_value;
+    using Valuable::is_valued;
     typedef Vertex V;
-
-    bool directed;
     
     // constructor, extends from std::pair and Valuable
     // gets two Vertex objects
-    Edge (V __v1, V __v2) : directed(false), std::pair<V, V>(__v1, __v2), Valuable() {};
+    Edge (V &__v1, V &__v2) : std::pair<V, V>(__v1, __v2), Valuable() {};
+    
+    // constructor getting two int ids
+    Edge (int __v1, int __v2) : std::pair<V, V>(Vertex(__v1), Vertex(__v2)), Valuable() {};
 
     ~Edge () {};
 
